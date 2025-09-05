10485906c5ed028f7509c16951a3b6e12e58a4ef
Damaris Poppe <damariscecilia29@gmail.com>
2025-09-05 02:47:21 -0600

    fixed error remaining files from deleting unused files

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,6 @@
 // querySelector mount points and call function to render stuff
 import { Hero } from './components/hero.js';
 import { Project } from './components/project.js';
-import { Social } from './components/socials.js';
-import { Post } from './components/blog.js';
 import { Nav } from './components/nav.js';
 import { FunProject } from './components/fun.js';
 import { Sparkles } from './components/sparkles.js';
@@ -130,41 +128,3 @@ async function initializeProjects() {
 }
 
 initializeProjects();
-
-
-
-
-//Render blog posts  array
-// Consider this syntax for now
-// const blogPosts = [
-//     {
-//         title: "Understanding Big O Notation",
-//         description: "A beginner-friendly explanation of time and space complexity...",
-//         date: "2025-08-16",
-//         category: "DSA"
-//     }
-// ];
-
-const posts = [
-    new Post("My first blog post", "This is a description of a blog post.","2025-08-15", "Tech"),
-    new Post("Learning DSA", "This is a description of a blog post.", "2025-08-18", "Fun"),
-    new Post("Takeaway of System Design", "This is a description of a blog post.", "2025-08-16", "Art"),
-];
-
-Post.renderBlogPosts(posts);
-
-const sortButton_date = document.getElementById("sort-date");
-sortButton_date.addEventListener('click', () => {
-    const sortedPosts_date = Post.SortbyDate(posts);
-    const blogDOM = document.querySelector("#blog");
-    blogDOM.innerHTML = '';
-    Post.renderBlogPosts(sortedPosts_date);
-});
-
-const sortButton_category = document.getElementById("sort-category");
-sortButton_category.addEventListener('click', () => {
-    const sortedPosts_category = Post.SortbyCategory(posts);
-    const blogDOM = document.querySelector("#blog");
-    blogDOM.innerHTML = '';
-    Post.renderBlogPosts(sortedPosts_category);
-});
