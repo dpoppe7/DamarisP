e86e71def0279f68a7bb83eeee18d28ea6ad6891
Damaris Poppe <damariscecilia29@gmail.com>
2025-09-06 15:55:21 -0600

    separeated config for netlify functions (server side) and client config (ES modules)

--- src/js/config/app-config.js
+++ src/js/config/app-config.js
@@ -1,25 +1,5 @@
-// config.js: 
+// Client config (ES modules)
 export const config = {
-    // github configuration
-    github: {
-        username: 'dpoppe7',
-        // image Paths: location in a github repo contwining the "display" image for such project, image that shows on carrousel (project.js)
-        imagePaths: ['images/display1.png', 'images/preview.png'], 
-        defaultBranch: 'main'
-    },
-
-    // API
-    api: {
-        endpoints: {
-            pinnedRepos: '/.netlify/functions/get-pinned-repos',
-            allRepos: '/.netlify/functions/get-all-repos'
-        },
-        limits: {
-            pinnedRepos: 6,
-            allRepos: 20,
-            topicsPerRepo: 8
-        }
-    },
 
     // Personal Information
     personal: {
@@ -64,21 +44,5 @@ export const config = {
     // Usage: fun.js
     filters: {
         funProjectTag: 'fun'
-    },
-
-    // Default Values
-    defaults: {
-        description: 'No description available',
-        fallbackUrl: '#',
-        imageAlt: 'Project preview',
-        repoTopicsLimit: 8 // matching the API response 'topicsPerRepo: 8'
     }
-};
-
-// Utility functions to get the configuration
-export const getConfig = {
-
-    // Get API URL
-    apiUrl: (endpoint) => config.api.endpoints[endpoint],
-
 };
